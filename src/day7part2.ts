7bacc6253642467f067e9c4808cd2d4e8214211c
alexbft <alexbft@users.noreply.github.com>
2023-04-09 15:52:42 +0200

    day7

---
+++ src/day7part2.ts
@@ -0,0 +1,249 @@
+import { openInputFile } from './lib/ts-bootstrap';
+
+class Instruction {
+    constructor(
+        public code: number,
+        public immediateMode: boolean[]) { }
+
+    readParams(program: number[], startAddr: number): number[] {
+        let numParams: number;
+        const immediateMode = [...this.immediateMode];
+        const needCheckAddress = immediateMode.map(_ => false);
+        if (this.code === 1 || this.code === 2 || this.code === 7 || this.code === 8) {
+            numParams = 3;
+            // We need to return the address directly, so I need to disable the indirection.
+            immediateMode[2] = true;
+            needCheckAddress[2] = true;
+        } else if (this.code === 3 || this.code === 4) {
+            numParams = 1;
+            if (this.code === 3) {
+                // Same, this is an address which is returned without indirection.
+                immediateMode[0] = true;
+                needCheckAddress[0] = true;
+            }
+        } else if (this.code === 5 || this.code === 6) {
+            numParams = 2;
+            needCheckAddress[1] = true;
+        } else {
+            throw new Error(`Can't read params for instruction ${this.code}`);
+        }
+        //console.log(`Executing@${startAddr - 1}: ${JSON.stringify(program.slice(startAddr - 1, startAddr + numParams))}`);
+        const result: number[] = [];
+        for (let i = 0; i < numParams; ++i) {
+            if (startAddr + i >= program.length) {
+                throw new Error(`Expected a parameter, got EOF`);
+            }
+            const valueOrAddr = program[startAddr + i];
+            let value: number;
+            if (immediateMode[i]) {
+                value = valueOrAddr;
+            } else {
+                if (valueOrAddr < 0 || valueOrAddr >= program.length) {
+                    throw new Error(`Invalid position`);
+                }
+                value = program[valueOrAddr];
+            }
+            if (needCheckAddress[i]) {
+                if (value < 0 || value >= program.length) {
+                    console.warn(`Invalid address at ${startAddr + i}: ${value}`);
+                }
+            }
+            result.push(value);
+        }
+        return result;
+    }
+}
+
+function parseInstruction(x: number): Instruction {
+    const code = x % 100;
+    x = x / 100 | 0;
+    const immediateMode: boolean[] = [];
+    for (let i = 0; i < 3; ++i) {
+        const digit = x % 10;
+        if (digit !== 0 && digit !== 1) {
+            throw new Error(`Can't parse instruction: ${x}`);
+        }
+        immediateMode.push(digit === 1);
+        x = x / 10 | 0;
+    }
+    return new Instruction(code, immediateMode);
+}
+
+class Computer {
+    constructor(private program: number[]) { }
+
+    run(inputs: number[]): number[] {
+        return new ComputerInstance(this.program, inputs).run();
+    }
+
+    getInstance() {
+        return new ComputerInstance(this.program, []);
+    }
+}
+
+class ComputerInstance {
+    private program: number[];
+    public inputs: number[];
+    private iptr: number = 0;
+    public isHalted: boolean = false;
+
+    constructor(program: number[], inputs: number[]) {
+        this.program = [...program];
+        this.inputs = [...inputs];
+    }
+
+    executeNextInstruction(): number | null {
+        if (this.isHalted) {
+            throw new Error('halted');
+        }
+        let output: number | null = null;
+        const instruction = parseInstruction(this.program[this.iptr]);
+        let params: number[] = [];
+        if (instruction.code !== 99) {
+            params = instruction.readParams(this.program, this.iptr + 1);
+        }
+        switch (instruction.code) {
+            case 1: { // addition
+                this.program[params[2]] = params[0] + params[1];
+                this.iptr += 4;
+                break;
+            }
+            case 2: { // multiplication
+                this.program[params[2]] = params[0] * params[1];
+                this.iptr += 4;
+                break;
+            }
+            case 3: { // store input value
+                if (this.inputs.length === 0) {
+                    throw new Error(`Empty input queue`);
+                }
+                this.program[params[0]] = this.inputs.shift()!;
+                this.iptr += 2;
+                break;
+            }
+            case 4: { // output
+                //console.log(`Output: ${params[0]}`);
+                output = params[0];
+                this.iptr += 2;
+                break;
+            }
+            case 5: { // jump if true
+                if (params[0] !== 0) {
+                    this.iptr = params[1];
+                } else {
+                    this.iptr += 3;
+                }
+                break;
+            }
+            case 6: { // jump if false
+                if (params[0] === 0) {
+                    this.iptr = params[1];
+                } else {
+                    this.iptr += 3;
+                }
+                break;
+            }
+            case 7: { // less than
+                const result = params[0] < params[1] ? 1 : 0;
+                this.program[params[2]] = result;
+                this.iptr += 4;
+                break;
+            }
+            case 8: { // equals
+                const result = params[0] === params[1] ? 1 : 0;
+                this.program[params[2]] = result;
+                this.iptr += 4;
+                break;
+            }
+            case 99: { // halt
+                this.isHalted = true;
+                break;
+            }
+            default:
+                throw new Error(`Unknown opcode: ${instruction.code}`);
+        }
+        return output;
+    }
+
+    run(): number[] {
+        const output: number[] = [];
+        while (!this.isHalted) {
+            const maybeOutput = this.executeNextInstruction();
+            if (maybeOutput != null) {
+                output.push(maybeOutput);
+            }
+        }
+        return output;
+    }
+
+    runUntilOutput(): number | null {
+        while (!this.isHalted) {
+            const maybeOutput = this.executeNextInstruction();
+            if (maybeOutput != null) {
+                return maybeOutput;
+            }
+        }
+        return null;
+    }
+}
+
+function permute(permutation: number[]) {
+    var length = permutation.length,
+        result = [permutation.slice()],
+        c: number[] = new Array(length).fill(0),
+        i = 1,
+        k: number,
+        p: number;
+
+    while (i < length) {
+        if (c[i] < i) {
+            k = i % 2 && c[i];
+            p = permutation[i];
+            permutation[i] = permutation[k];
+            permutation[k] = p;
+            ++c[i];
+            i = 1;
+            result.push(permutation.slice());
+        } else {
+            c[i] = 0;
+            ++i;
+        }
+    }
+    return result;
+}
+
+async function main() {
+    const f = await openInputFile();
+    let programNum = 0;
+    const phasePermutations = permute([5, 6, 7, 8, 9]);
+    for await (const programRaw of f.readLines()) {
+        const program = programRaw.split(',').map(Number);
+        console.log(`Program ${programNum++}`);
+        const computer = new Computer(program);
+        let maxSignal = 0;
+        for (const phases of phasePermutations) {
+            const instances = phases.map(phase => {
+                const instance = computer.getInstance();
+                instance.inputs.push(phase);
+                return instance;
+            });
+            let signal: number | null = 0;
+            let lastSignal: number = 0;
+            while (signal != null) {
+                for (const instance of instances) {
+                    if (signal != null) {
+                        instance.inputs.push(signal);
+                        lastSignal = signal;
+                    }
+                    signal = instance.runUntilOutput();
+                }
+            }
+            if (maxSignal < lastSignal) {
+                maxSignal = lastSignal;
+            }
+        }
+        console.log(`max signal: ${maxSignal}`);
+    }
+}
+
+main();
